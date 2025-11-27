aba779966927123ef976e182a3729b421f396bb0
jdguior06 <j.d.guior010602@gmail.com>
2025-11-26 21:18:13 -0400

    prueba

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production'
-        ? '/inf513/grupo21sc/proyecto2/tec/public/'
-        : '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue(),
-    ]
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    base: '/inf513/grupo21sc/proyecto2/tec/public/build/',
 });
