d79e901cd85507c29283aa23971572d48581c8ce
jdguior06 <j.d.guior010602@gmail.com>
2025-11-26 20:58:39 -0400

    despliegue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: process.env.NODE_ENV === 'production'
+        ? '/inf513/grupo21sc/proyecto2/tec/public/'
+        : '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
